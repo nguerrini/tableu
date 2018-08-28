2862e5914d8e9118e330056284d1892b71257050
Nicola Guerrini <nicola.guerrini@gmail.com>
2018-08-28 22:09:53 +0200

    Initial repo commit

---
+++ Index.js
@@ -0,0 +1,38 @@
+{"name":"Prendete posto e iniziate la festa!",children:
+
+[{name:"Watson e Crick",children:
+[{name:"Zia Stefania"},{name:"Zio Riccardo"},{name:"Zia Esa"},{name:"Zio Orlando"},{name:"Matteo"},{name:"Simona"},{name:"Pietro"},{name:"Ginetta"},{name:"Orcar"}]},
+
+{name:"Comte e Durkheim",children:
+[{name:"Zia Daniela",size:17010},{name:"Roberta",size:5842},{name:"Mattia"},{name:"Camilla",size:1041},{name:"Tommaso",size:5176},{name:"Filippo",size:449},{name:"Zio Marco",size:5593},{name:"Luisa",size:5534}]},
+
+{name:"Babbage e Lovelace",children:
+[{name:"Mamma Ornella"},{name:"Papà Enrico",size:1759},{name:"Paola",size:2165},{name:"Stefano",size:586},{name:"Filippo",size:3331},{name:"Lorenzo",size:772},{name:"Alessandro",size:3322},{name:"Eleonora",size:3322}]},
+
+{name:"Wirth e Van Rossum",children:
+[{name:"Giorgio P.",size:8833},{name:"Elisabetta Z.",size:1732},{name:"Francesca",size:3623},{name:"Ezio",size:10066},{name:"Aurora",size:10066},{name:"Matilde",size:10066},{name:"Laura",size:10066},{name:"Tiberio",size:10066}]},
+
+[{name:"Pierre e Marie Curie",children:
+[{name:"Mamma Giovanna"},{name:"Papà Maurizio"},{name:"Daniela C."},{name:"Paolo F."},{name:"Zio Dino"},{name:"Zia Rosa"},{name:"Roberto"},{name:"Antonella"},{name:"Dino"},{name:"Giusy"}]},
+
+[{name:"De Saussure e Chomsky",children:
+[{name:"Lucia"},{name:"Salvatore"},{name:"Ivonne"},{name:"Giuseppe"},{name:"Antonietta"},{name:"Leo"},{name:"Nuccia"}]},
+
+[{name:"Le Cun e Bengio",children:
+[{name:"Betta"},{name:"Luca(Faina)"},{name:"Silvia F."},{name:"Edo"},{name:"Ricky"},{name:"One"},{name:"Anna"},{name:"Alice"}]},
+
+[{name:"Norman e Shallice",children:
+[{name:"Luca B."},{name:"Paola Q."},{name:"Andrea B."},{name:"Efrem"},{name:"Alessia"},{name:"Andrea(Meno)"},{name:"Alice"},{name:"Darietto"},{name:"Gloria"}]},
+
+[{name:"Berners Lee e Cailliau",children:
+[{name:"Sayaka"},{name:"Shintaro"},{name:"Fabrizio(Beta)"},{name:"Chiara"},{name:"Alessandro(Sotu)"},{name:"Albe"},{name:"Fernanda"},{name:"Enrico(Monzo)"}]},
+
+[{name:"Kerninghan e Ritchie",children:
+[{name:"Enrico B."},{name:"Elisa B."},{name:"Faby"},{name:"Sante"},{name:"Michela M."},{name:"Nicole"},{name:"Figlio M."},{name:"Vale"},{name:"Isa"},{name:"Antonio"}]},
+
+[{name:"Shannon e Weaver",children:
+[{name:"Dadinne"},{name:"J"},{name:"Giorgio C."},{name:"Andrea T."},{name:"Andre"},{name:"Silviett"},{name:"Damianino"},{name:"Alice"}]},
+
+[{name:"Turing e Clarke",children:
+[{name:"Lorenzo C."},{name:"Takako"},{name:"Gufo"},{name:"Michela G."},{name:"Martino"},{name:"Nico"},{name:"Luca(Cailu)"},{name:"Vera"}]}
+};
